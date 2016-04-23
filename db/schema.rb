401ad0b6c0d5c0061acf0914327d69b9e7d582ce
Xander Miller <xander.miller@gmail.com>
2016-04-23 00:54:52 +0000

    assignment models use shoulda specs and migration changes resolved options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422204219) do
+ActiveRecord::Schema.define(version: 20160423004606) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 20160422204219) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "resolved",   default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
