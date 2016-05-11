088cb900a3ef116a44d84892676fb0b0e770783b
matt <mhwnotes@icloud.com>
2016-05-11 19:11:53 -0400

    42_assignment alpha

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,30 +3,28 @@ class CommentsController < ApplicationController
   before_action :authorize_user, only: [:destroy]
 
   def create
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
+    @parent = get_parent()
+    comment = @parent.comments.new(comment_params)
     comment.user = current_user
 
     if comment.save
       flash[:notice] = "Comment saved successfully."
-      redirect_to [@post.topic, @post]
     else
       flash[:alert] = "Comment failed to save."
-      redirect_to [@post.topic, @post]
     end
+    redirect_to get_redirect_to(comment)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    @parent = get_parent()
+    comment = @parent.comments.find(params[:id])
 
     if comment.destroy
       flash[:notice] = "Comment was deleted successfully."
-      redirect_to [@post.topic, @post]
     else
       flash[:alert] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
     end
+    redirect_to get_redirect_to(comment)
   end
 
   private
@@ -39,7 +37,23 @@ class CommentsController < ApplicationController
     comment = Comment.find(params[:id])
     unless current_user == comment.user || current_user.admin?
       flash[:alert] = "You do not have permission to delete a comment."
-      redirect_to [comment.post.topic, comment.post]
+      redirect_to get_redirect_to(comment)
+    end
+  end
+
+  def get_parent
+    if params[:topic_id]
+      return Topic.find(params[:topic_id])
+    elsif params[:post_id]
+      return Post.find(params[:post_id])
+    end
+  end
+
+  def get_redirect_to(comment)
+    if comment.post
+      return [comment.post.topic, comment.post]
+    else
+      return comment.topic
     end
   end
 
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,12 +2,4 @@ module CommentsHelper
   def user_is_authorized_for_comment?(comment)
     current_user && (current_user == comment.user || current_user.admin?)
   end
-
-  # def get_comment_parent(comment)
-  #   if comment.topic_id?
-  #     return comment.topic
-  #   elsif comment.post_id?
-  #     return comment.post
-  #   end
-  # end
 end
