1167074c455983c6ca22c205a68ef8fed028d746
Xander Miller <xander.miller@gmail.com>
2016-05-11 17:05:52 +0000

    refactors post specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,11 +12,15 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:topic) }
   end
   
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:body) }
-  it { is_expected.to validate_presence_of(:topic) }
-
-  it { is_expected.to validate_length_of(:title).is_at_least(5) }
-  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  describe 'validations' do
+    # title  
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_least(5) }
+    # body
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(20) }
+    # topic
+    it { should validate_presence_of(:topic) }
+  end
 
 end
