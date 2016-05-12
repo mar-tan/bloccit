a97315a7d938e1c35d9ea45738d5c65d15e991ca
Xander Miller <xander.miller@gmail.com>
2016-05-12 14:33:28 +0000

    refactors user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
 
-  before_save :parse_user
-
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: {minimum: 6}, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
@@ -9,12 +7,21 @@ class User < ActiveRecord::Base
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
+
+  # You should keep methods simple, one method should do one thing.
+  before_save :scrub_email
+  before_save :scrub_name
+
   has_secure_password
 
-  def parse_user
+  def scrub_email
     # normalize email to lowercase
     self.email.downcase! if self.email
+  end
+  
+  def scrub_name
     # make name look right
     self.name = self.name.split(" ").map { |s| s.capitalize }.join(" ") if self.name
   end
+
 end
