e612bc733c9848bfe1ee8e8adec4cf7d1bd740ad
matt <mhwnotes@icloud.com>
2016-05-15 12:25:20 -0400

    42_assignment sunday best

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,7 +66,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_length_of(:email).is_at_least(3) }
     it { is_expected.to allow_value("user@bloccit.com").for(:email) }
-    # password
+    # password 
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to have_secure_password }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
