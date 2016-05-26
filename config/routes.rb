613bf6c8c94021979ffe4702399d72dda2fe57c7
matt <mhwnotes@icloud.com>
2016-05-26 12:11:07 -0400

    48_checkpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,13 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
+
   get 'welcome/contact'
 
 end
