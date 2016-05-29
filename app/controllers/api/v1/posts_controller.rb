0fbe23961107d9cfa751582496ee6d8b393563f2
matt <mhwnotes@icloud.com>
2016-05-29 15:31:40 -0400

    49_assignment nowhere fast

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -23,6 +23,6 @@ class Api::V1::PostsController < Api::V1::BaseController
 
   private
   def post_params
-    params.require(:title).permit(:body, :topic, :user)
+    params.require(:id).permit(:title, :body, :topic_id, :user_id)
   end
 end
--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -7,12 +7,12 @@ require 'rails_helper'
 
   context "unauthenticated user" do
     it "PUT update returns http unauthenticated" do
-      put :update, id: my_post.id, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic: my_topic, user: my_user }
+      put :update, id: my_post.id, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic_id: my_topic.id, user_id: my_user.id }
       expect(response).to have_http_status(401)
     end
 
     it "POST create returns http unauthenticated" do
-      post :create, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic: my_topic, user: my_user }
+      post :create, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic_id: my_topic.id, user_id: my_user.id }
       expect(response).to have_http_status(401)
     end
 
@@ -24,12 +24,12 @@ require 'rails_helper'
 
   context "unauthorized user" do
     it "PUT update returns http forbidden" do
-      put :update, id: my_post.id, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic: my_topic, user: my_user }
+      put :update, id: my_post.id, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic_id: my_topic.id, user_id: my_user.id }
       expect(response).to have_http_status(403)
     end
 
     it "POST create returns http forbidden" do
-      post :create, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic: my_topic, user: my_user }
+      post :create, post: {title: "Post Title", body: "There once was a frog who lived in a bog and he played the bass fiddle in the middle of a puddle.", topic_id: my_topic.id, user_id: my_user.id }
       expect(response).to have_http_status(403)
     end
 
